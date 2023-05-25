b02d34ba5891aecbd21fe40e1f2f5ec3d2e420b4
Hamad Badar <hamadbadar5626@hotmail.com>
2023-05-25 12:52:41 +0500

    Resolved Media Functionality

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,38 +1,52 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import ChatMessage
+
 class Consumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        # Join room group
+
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
         self.accept()
+
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
-    # Receive message from WebSocket
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        # Send message to room group
+        message = text_data_json.get('message', '')
+        media = text_data_json.get('media', '')
+
+        print('Media: ==> ', media, '\nMessage: ==>', message)
+
+        # Save the received data to the database
+        chat_message = ChatMessage.objects.create(message=message, media=media)
+        chat_message.save()
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'media': media,
             }
         )
-    # Receive message from room group
+
     def chat_message(self, event):
-        message = event['message']
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        message = event.get('message', '')
+        media = event.get('media', '')
+
+        data = {}
+        if message:
+            data['message'] = message
+        if media:
+            data['media'] = media
+
+        self.send(text_data=json.dumps(data))
--- chatapp/models.py
+++ chatapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ChatMessage(models.Model):
     message = models.TextField()
     media = models.FileField(upload_to='media/', blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"ChatMessage {self.id}"
+        return f"ChatMessage {self.pk}"
